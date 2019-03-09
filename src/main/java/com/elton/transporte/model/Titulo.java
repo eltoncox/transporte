f807d61d64de0c08ba5582641ba17aa85efdb4a8
eltoncox <elton.unix@gmail.com>
2019-03-08 23:58:59 -0300

    acertando as classes

--- src/main/java/com/elton/transporte/model/Titulo.java
+++ src/main/java/com/elton/transporte/model/Titulo.java
@@ -16,7 +16,6 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
