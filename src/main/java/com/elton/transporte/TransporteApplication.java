a3dce74b03436820029bb648ed8a95518a4b615f
eltoncox <elton.unix@gmail.com>
2019-03-09 12:20:17 -0300

    retornando a configuração anterior

--- src/main/java/com/elton/transporte/TransporteApplication.java
+++ src/main/java/com/elton/transporte/TransporteApplication.java
@@ -29,7 +29,7 @@ public class TransporteApplication {
 	 
 		@Override
 		public void addViewControllers(ViewControllerRegistry registry) {
-			registry.addRedirectViewController("/", "titulos");
+			registry.addRedirectViewController("/", "/titulos");
 		}		
 	}
 }
